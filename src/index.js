d9947e730a5ccf1ba57176c7d3f21598427cc64c
Nik <nikhillukemathew@gmail.com>
2019-01-22 00:42:57 +0530

    testing firebase login

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
 
 // Firebase App is always required and must be first
-var firebase = require("firebase/app");
-
+// var firebase = require("firebase/app");
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 // Add additional services that you want to use
-require("firebase/auth");
+// require("firebase/auth");
 // require("firebase/database");
-require("firebase/firestore");
+// require("firebase/firestore");
 // require("firebase/messaging");
 // require("firebase/functions");
 
@@ -26,6 +27,23 @@ var config = {
     messagingSenderId: "479296809558"
   };
 firebase.initializeApp(config);
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      console.log("user is signed in ",user)
+    } else {
+      // No user is signed in.
+    }
+  });
+// firebase.auth().createUserWithEmailAndPassword('hello@abc.com','12345R')
+// .then(result=>console.log(result))
+// .catch(function(error) {
+//     // Handle Errors here.
+//     // var errorCode = error.code;
+//     // var errorMessage = error.message;
+//     // ...
+//     console.log(error)
+//   });
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
